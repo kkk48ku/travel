264b561a3fa1bd39d0f7cfb647b551515e778568
yumi <yikeyumi65@gmail.com>
2019-06-26 12:46:33 +0800

    change

---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="wrapper">
+    <swiper :options="swiperOption">
+      <swiper-slide v-for="item of slideUrlList" :key="item.id">
+        <img class="swiper-img" :src="item.url" />
+      </swiper-slide>
+      <div class="swiper-pagination" slot="pagination"></div>
+    </swiper>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeSwiper',
+  data () {
+    return {
+      swiperOption: {
+        pagination: '.swiper-pagination',
+        loop: true
+      },
+      slideUrlList: [
+        {
+          id: '0001',
+          url: 'http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20196/b421363bbbb9184deacb4247eeea7485.jpg_750x200_12b42643.jpg'
+        },
+        {
+          id: '0002',
+          url: 'http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20196/818f6cc784ae6669b74bbbb255414a53.jpg_750x200_66ca5873.jpg'
+        },
+        {
+          id: '0003',
+          url: 'http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20193/87a224d0349d94a11e97f31aa1aba4f5.jpg_750x200_1f78af87.jpg'
+        },
+        {
+          id: '0004',
+          url: 'http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20196/cdf8e74f9c7dfb886af145efc69cf6e2.jpg_750x200_4db2ecac.jpg'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+.wrapper >>> .swiper-pagination-bullet-active {
+  background: #fff;
+}
+
+.wrapper {
+  width: 100%;
+  height: 0;
+  overflow: hidden;
+  padding-bottom: 26.5%;
+  background-color: #eee;
+
+  .swiper-img {
+    width: 100%;
+  }
+}
+</style>
